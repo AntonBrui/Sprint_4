a9c37cfed8d9748903c2e8ff3a3b42ca4dfbd195
AntonBrui <BruiAnton@yandex.by>
2022-04-26 11:44:26 +0200

    Sprint_4

---
+++ src/main/java/Praktikum.java
@@ -0,0 +1,8 @@
+public class Praktikum {
+
+    public static void main(String[] args) {
+        String name = "Anton Brui";
+        Account account = new Account(name);
+        account.checkNameToEmboss();
+    }
+}
